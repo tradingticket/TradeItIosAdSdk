22e236b027d9a74475c42008a13ef0011bf576a3
James Robert Somers <james@trade.it>
2016-07-28 15:35:36 -0400

    Hello framework

--- TradeItIosAdSdk/TradeItIosAdSdk.h
+++ TradeItIosAdSdk/TradeItIosAdSdk.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double TradeItIosAdSdkVersionNumber;
 FOUNDATION_EXPORT const unsigned char TradeItIosAdSdkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TradeItIosAdSdk/PublicHeader.h>
-
-
---
+++ TradeItIosAdSdk/TradeItIosAdView.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+@objc public class TradeItIosAdView : NSObject {
+    public override init() {
+        print("Hello from TradeItIosAdView")
+    }
+
+    public func doSomething() {
+        print("Doing something")
+    }
+}
