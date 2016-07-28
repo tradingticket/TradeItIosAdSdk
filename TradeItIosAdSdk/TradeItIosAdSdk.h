a70ea5600441015ddaa08302bd57982fb7f028ac
James Robert Somers <james@trade.it>
2016-07-28 15:31:14 -0400

    Create Framework

---
+++ TradeItIosAdSdk/TradeItIosAdSdk.h
@@ -0,0 +1,19 @@
+//
+//  TradeItIosAdSdk.h
+//  TradeItIosAdSdk
+//
+//  Created by James Robert Somers on 7/28/16.
+//  Copyright © 2016 TradeIt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TradeItIosAdSdk.
+FOUNDATION_EXPORT double TradeItIosAdSdkVersionNumber;
+
+//! Project version string for TradeItIosAdSdk.
+FOUNDATION_EXPORT const unsigned char TradeItIosAdSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TradeItIosAdSdk/PublicHeader.h>
+
+
