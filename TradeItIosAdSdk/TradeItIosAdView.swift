9f3b17d9ca8c6be9e7d8065ccb54b47fb134db76
James Robert Somers <james@trade.it>
2016-07-29 11:21:07 -0400

    Attaching a Nib with a WebView

--- TradeItIosAdSdk/TradeItIosAdView.swift
+++ TradeItIosAdSdk/TradeItIosAdView.swift
@@ -1,11 +1,29 @@
 import UIKit
 
-@objc public class TradeItIosAdView : NSObject {
-    public override init() {
-        print("Hello from TradeItIosAdView")
+public class TradeItIosAdView: UIView {
+    @IBOutlet var view: UIView!
+    @IBOutlet weak var webView: UIWebView!
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+        loadContent()
     }
 
-    public func doSomething() {
-        print("Doing something")
+    func xibSetup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
+        addSubview(view)
+    }
+
+    func loadViewFromNib() -> UIView {
+        return NSBundle(forClass: self.dynamicType).loadNibNamed("TradeItIosAdView", owner: self, options: nil)[0] as! UIView
+    }
+
+    func loadContent() {
+        let url = NSURL(string: "https://www.trade.it")
+        let requestObj = NSURLRequest(URL: url!)
+        webView.loadRequest(requestObj);
     }
 }
