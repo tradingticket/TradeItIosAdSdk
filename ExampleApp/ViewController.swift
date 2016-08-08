cd2fc24f169e4a9af39cdda29c63b88c9f1de513
James Robert Somers <james@trade.it>
2016-08-08 16:21:06 -0400

    Add Example App

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var adView: TradeItAdView!
+    @IBOutlet weak var adViewHeightConstraint: NSLayoutConstraint!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TradeItAdConfig.debug = true
+        TradeItAdConfig.apiKey = "tradeit-test-api-key"
+        TradeItAdConfig.environment = .QA
+        self.adView.configureWithAdType("general", heightConstraint: self.adViewHeightConstraint)
+    }
+}
+
